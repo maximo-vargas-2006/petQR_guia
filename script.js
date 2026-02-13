aebf00c8f59a8617a89e35f190f2a33e2ae5e247
maximo-vargas-2006 <maximovarga123@gmail.com>
2026-02-13 16:50:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-function ejemploWhatsApp() {
-  const mensaje =
-    "Hola 👋\n" +
-    "Este es un ejemplo de cómo PetQR permite contactar al dueño.\n" +
-    "Con un solo toque, se envía la ubicación.";
+function enviarWhatsApp() {
+  const nombre = document.getElementById("nombre").value;
+  const tipo = document.getElementById("tipo").value;
+  const tamano = document.getElementById("tamano").value;
+  const zona = document.getElementById("zona").value;
+  const telefonoContacto = document.getElementById("telefono").value;
+  const obs = document.getElementById("obs").value;
 
-  const url = "https://wa.me/5491112345678?text=" + encodeURIComponent(mensaje);
+  const mensaje =
+    "¡Hola! 👋 Quiero solicitar mi PetQR 🐾\n\n" +
+    "Datos de mi mascota:\n" +
+    "• Nombre: " + nombre + "\n" +
+    "• Tipo: " + tipo + "\n" +
+    "• Tamaño de chapa: " + tamano + "\n" +
+    "• Zona: " + zona + "\n" +
+    "• Teléfono: " + telefonoContacto + "\n" +
+    "• Observaciones: " + obs + "\n\n" +
+    "Quedo a la espera para coordinar 🙂";
+
+  const telefonoVendedor = "5491144408187"; // TU NÚMERO
+
+  const url = "https://wa.me/" + telefonoVendedor + "?text=" + encodeURIComponent(mensaje);
+
   window.open(url, "_blank");
 }
